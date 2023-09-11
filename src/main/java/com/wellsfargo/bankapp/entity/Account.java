f3cb7bd3dfe62ee122c703f7f952424af9d69b36
saipranavvv <bpranav89@gmail.com>
2023-09-11 15:35:30 +0530

    Updated all the entities to remove errors

--- src/main/java/com/wellsfargo/bankapp/entity/Account.java
+++ src/main/java/com/wellsfargo/bankapp/entity/Account.java
@@ -1,93 +1,116 @@
 package com.wellsfargo.bankapp.entity;
 
-package com.wellsfargo.bankapp.Records;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.List;
 @Entity
 @Table
 public class Account {
     @Id
     @Column
-    private String accountno;
+    private String accountNo;
     @Column
-    private String accounttype;
+    private String accountType;
     @Column
     private int balance;
     @Column
-    private String userid;
+    private String userId;
     @Column
     private String branch;
     @Column
-    private LocalDate activationdate;
+    private LocalDate activationDate;
     @Column
-    private LocalDate deactivationdate;
-    public AccountsInfoRecord(String accountno, String accounttype, int balance, String userid, String branch, LocalDate activationdate, LocalDate deactivationdate) {
-        this.accountno = accountno;
-        this.accounttype = accounttype;
-        this.balance = balance;
-        this.userid = userid;
-        this.branch = branch;
-        this.activationdate = activationdate;
-        this.deactivationdate = deactivationdate;
-    }
-    public AccountsInfoRecord() {
-    }
-    public String getAccountno() {
-        return accountno;
-    }
-    public void setAccountno(String accountno) {
-        this.accountno = accountno;
-    }
-    public String getAccounttype() {
-        return accounttype;
-    }
-    public void setAccounttype(String accounttype) {
-        this.accounttype = accounttype;
-    }
-    public int getBalance() {
-        return balance;
-    }
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-    public String getUserid() {
-        return userid;
-    }
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-    public String getBranch() {
-        return branch;
-    }
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
-    public LocalDate getActivationdate() {
-        return activationdate;
-    }
-    public void setActivationdate(LocalDate activationdate) {
-        this.activationdate = activationdate;
-    }
-    public LocalDate getDeactivationdate() {
-        return deactivationdate;
-    }
-    public void setDeactivationdate(LocalDate deactivationdate) {
-        this.deactivationdate = deactivationdate;
-    }
-    @Override
-    public String toString() {
-        return "AccountsInfoRecord{" +
-                "accountno='" + accountno + '\'' +
-                ", accounttype='" + accounttype + '\'' +
-                ", balance=" + balance +
-                ", userid='" + userid + '\'' +
-                ", branch='" + branch + '\'' +
-                ", activationdate=" + activationdate +
-                ", deactivationdate=" + deactivationdate +
-                '}';
-    }
+    private LocalDate deactivationDate;
+    
+    @ManyToOne
+    @JoinColumn(name="id")
+    private Customer user;
+    
+    public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public LocalDate getActivationDate() {
+		return activationDate;
+	}
+
+	public void setActivationDate(LocalDate activationDate) {
+		this.activationDate = activationDate;
+	}
+
+	public LocalDate getDeactivationDate() {
+		return deactivationDate;
+	}
+
+	public void setDeactivationDate(LocalDate deactivationDate) {
+		this.deactivationDate = deactivationDate;
+	}
+
+	public Customer getUser() {
+		return user;
+	}
+
+	public void setUser(Customer user) {
+		this.user = user;
+	}
+
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
+	@OneToMany(mappedBy="accountDetail",fetch=FetchType.EAGER,cascade=CascadeType.ALL)
+    private List<Transaction> transactions;
+
+	
+    
+    
 }
 
