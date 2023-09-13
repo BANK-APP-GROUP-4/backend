75e62be9bfb64f0a5de6b83ca51c46177564216f
SharanPrasath <sharanprasath3008@gmail.com>
2023-09-13 09:05:08 +0530

    changes

--- src/main/java/com/wellsfargo/bankapp/service/SavingsAccountService.java
+++ src/main/java/com/wellsfargo/bankapp/service/SavingsAccountService.java
@@ -2,6 +2,9 @@ package com.wellsfargo.bankapp.service;
 
 import com.wellsfargo.bankapp.entity.SavingsAccount;
 import com.wellsfargo.bankapp.repository.SavingsAccountRepo;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,13 @@ public class SavingsAccountService {
 
     void addSavingsAccount(SavingsAccount savingsAccount){
         savingsAccountRepo.save(savingsAccount);
+    }
+    
+    public SavingsAccount savingsAccount(Long id) {
+    	System.out.println(id);
+    	SavingsAccount savingsaccount = null;
+    	Optional<SavingsAccount> sa = savingsAccountRepo.findById(id);
+    	savingsaccount = sa.get();
+    	return savingsaccount;
     }
 }
