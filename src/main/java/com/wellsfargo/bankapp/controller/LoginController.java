478ea56163657820da3d746030eb2edf5c6bb62f
SharanPrasath <80667110+SharanPrasath@users.noreply.github.com>
2023-09-15 10:04:54 +0530

    Update LoginController.java

--- src/main/java/com/wellsfargo/bankapp/controller/LoginController.java
+++ src/main/java/com/wellsfargo/bankapp/controller/LoginController.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.simple.JSONObject;
 import com.wellsfargo.bankapp.entity.Customer;
 import com.wellsfargo.bankapp.entity.Login;
 import com.wellsfargo.bankapp.entity.SavingsAccount;
+import com.wellsfargo.bankapp.entity.Transaction;
+import com.wellsfargo.bankapp.repository.TransactionRepo;
 import com.wellsfargo.bankapp.service.SavingsAccountService;
 import com.wellsfargo.bankapp.validation.LoginValidation;
 
@@ -24,6 +31,9 @@ public class LoginController {
 	@Autowired
 	private SavingsAccountService savingsAccountService;
 	
+	@Autowired
+	private TransactionRepo transactionRepo;
+	
 	@PostMapping("/login")
     @CrossOrigin(origins = "http://localhost:3000")
 	public JSONObject validateuser(@RequestBody Login login) {
@@ -32,9 +42,13 @@ public class LoginController {
 		
 		if(customer!=null) {
 			SavingsAccount sa = savingsAccountService.savingsAccount(customer.getId());
+//			List<Long> id =Arrays.asList(sa.getId());
+
+			List<Transaction> transaction = transactionRepo.findBysender_acc(sa.getId());
 			System.out.println(sa.getActivationDate());
 			System.out.println(sa.getBalance());
 			JSONObject obj = new JSONObject();
+			System.out.println(customer);
 			obj.put("name", customer.getFirstName());
 			obj.put("email", customer.getEmail());
 			JSONObject obj2 = new JSONObject();
@@ -43,10 +57,10 @@ public class LoginController {
 			obj2.put("Account_Activation_Date",sa.getActivationDate());
 			payload.put("customer_details", obj);
 			payload.put("account_details", obj2);
+			payload.put("transaction_details", transaction);
 			payload.put("token", "12345");
 			payload.put("status", "Success");
 			payload.put("message","valid Creds");
-			
 			
 		}
 		else {
