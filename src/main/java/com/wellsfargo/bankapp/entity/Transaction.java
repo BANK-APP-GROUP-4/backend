1ed4391b8cbce3e885315d079bffee30846a58ee
SharanPrasath <80667110+SharanPrasath@users.noreply.github.com>
2023-09-15 10:07:24 +0530

    Update Transaction.java

--- src/main/java/com/wellsfargo/bankapp/entity/Transaction.java
+++ src/main/java/com/wellsfargo/bankapp/entity/Transaction.java
@@ -24,9 +24,62 @@ public class Transaction {
 	private LocalDate date;
 	@Column(name="receiver_acc")
 	private String receiver_acc;
-	@ManyToOne(targetEntity=SavingsAccount.class, cascade=CascadeType.ALL)
-	@JoinColumn(name="account_detail")
-	private List<Account> accountDetail;
+//	@ManyToOne(targetEntity=SavingsAccount.class, cascade=CascadeType.ALL)
+//	@JoinColumn(name="account_detail")
+//	private List<Account> accountDetail;
+	
+	@Column
+	private Long sender_acc;
+	
+	public Transaction(int amount, LocalDate date, String receiver_acc, List<Account> accountDetail,Long sender_acc) {
+		super();
+		this.amount = amount;
+		this.date = date;
+		this.receiver_acc = receiver_acc;
+		this.sender_acc = sender_acc;
+	}
+	
+	public Long getSender_acc() {
+		return sender_acc;
+	}
+
+	public void setSender_acc(Long sender_acc) {
+		this.sender_acc = sender_acc;
+	}
+
+	public Transaction() {}
+
+	public int getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(int transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getReceiver_acc() {
+		return receiver_acc;
+	}
+
+	public void setReceiver_acc(String receiver_acc) {
+		this.receiver_acc = receiver_acc;
+	}
 
 	
 	
