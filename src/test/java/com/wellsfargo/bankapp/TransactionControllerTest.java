e6af9b07d4bbb9931878ac544cc5b3a684870d49
saipranavvv <70323232+saipranavvv@users.noreply.github.com>
2023-10-02 20:20:44 +0530

    Create TransactionControllerTest.java

---
+++ src/test/java/com/wellsfargo/bankapp/TransactionControllerTest.java
@@ -0,0 +1,68 @@
+package com.wellsfargo.bankapp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.hamcrest.Matchers;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wellsfargo.bankapp.dto.CustomerDTO;
+import com.wellsfargo.bankapp.entity.Customer;
+import com.wellsfargo.bankapp.entity.account.SavingsAccount;
+import com.wellsfargo.bankapp.repository.CustomerRepo;
+import com.wellsfargo.bankapp.repository.SavingsAccountRepo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.wellsfargo.bankapp.service.CustomerService;
+import com.wellsfargo.bankapp.service.SavingsAccountService;
+import com.wellsfargo.bankapp.service.TransactionService;abstract
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class TransactionControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private TransactionService transactionService;
+	
+	
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	String jwt_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJocml0aWNrQGdtYWlsLmNvbSIsImlhdCI6MTY5NTk1OTc3MSwiZXhwIjoxNjk2MDQ2MTcxfQ.n1qEZ8RkzLE0j7GuV7KjdHNGW8m3y0eH-maq38t11NSZwbC0OvSaafvWh23bdz2Jp7UeeKuEiFI3ZTaQdLqLOQ";
+	
+	
+	@Test
+	public void testsendAmount() throws Exception {
+		Long senderAccId = 1L;
+        Long receiverAccId = 2L;
+        double amount = 200;
+        
+        JSONObject obj = new JSONObject();
+        obj.put("senderAccId", senderAccId);
+        obj.put("receiverAccId", receiverAccId);
+        obj.put(amount, 200);
+		String json = mapper.writeValueAsString(obj);
+		Mockito.when(transactionService.addTransaction(senderAccId,receiverAccId,amount)).thenReturn("Transaction Succesful!");
+		mvc.perform(post("api/v1/transaction/send").header("Authorization", "Bearer "+ jwt_token)
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+				.content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+	
+}
