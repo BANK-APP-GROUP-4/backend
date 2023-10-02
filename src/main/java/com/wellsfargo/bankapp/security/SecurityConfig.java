fbcd15d85676e385cb4b2c2b1c6d45653be0ddc8
saipranavvv <70323232+saipranavvv@users.noreply.github.com>
2023-10-02 20:03:43 +0530

    Update SecurityConfig.java
    sharan

--- src/main/java/com/wellsfargo/bankapp/security/SecurityConfig.java
+++ src/main/java/com/wellsfargo/bankapp/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.wellsfargo.bankapp.security;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.servlet.HttpSecurityDsl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
@@ -28,6 +29,10 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthenticationFilter filter;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -46,15 +51,16 @@ public class SecurityConfig {
     	
     }
     
+    
 	
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration corsConfiguration = new CorsConfiguration();
-    	corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-    	corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-    	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    	corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    	corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+    	corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
     	corsConfiguration.setAllowCredentials(true);
-    	corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+    	corsConfiguration.setExposedHeaders(List.of("Authorization"));
     	
     	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     	source.registerCorsConfiguration("/**", corsConfiguration);
